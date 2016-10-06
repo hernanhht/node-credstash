b06fa5906007f23e0f22b9f4d4ce83dbe7a5b0a8
Hernán Tierno <htierno@gmail.com>
2016-10-06 17:37:43 -0300

    Add credstash put method

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const defaults = {
 
 function Credstash(config) {
   this.table = config ? config.table : undefined;
+  this.kms_key = config ? config.kms.key : 'alias/credstash';
 }
 
 Credstash.prototype.get = function(name, options, done) {
@@ -34,6 +35,34 @@ Credstash.prototype.get = function(name, options, done) {
 
     if (options.limit === 1) {
       return done(null, secrets && secrets[0]);
+    }
+
+    done(null, secrets);
+  });
+};
+
+Credstash.prototype.put = function(name, value, version, options, done) {
+  if (typeof version === 'function') {
+    done = version;
+    version = null;
+    options = defaults;
+  }
+
+  if (typeof options === 'function') {
+    done = options;
+    options = defaults;
+  } else {
+    options = xtend(defaults, options);
+  }
+
+  return async.waterfall([
+    async.apply(keys.generate, this.kms_key),
+    async.apply(decrypter.encrypt, value),
+    async.apply(hmac.sign),
+    async.apply(secrets.put, this.table, name, version)
+  ], function (err, secrets) {
+    if (err) {
+      return done(err);
     }
 
     done(null, secrets);
